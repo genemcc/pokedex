71f14519ed7c09f1610ff963bd37ecc2482cae70
Gene McCullagh <gene@lightroomsecrets.com>
2015-11-29 06:49:41 -0600

    Added Pokemon class

---
+++ pokedex/Pokemon.swift
@@ -0,0 +1,27 @@
+//
+//  Pokemon.swift
+//  pokedex
+//
+//  Created by Gene McCullagh on 11/29/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import Foundation
+
+class Pokemon {
+    private var _name: String!
+    private var _pokedexId: Int!
+    
+    var name: String {
+        return _name
+    }
+    
+    var pokedexId: Int {
+        return _pokedexId
+    }
+    
+    init(name: String, pokedexId: Int) {
+        self._name = name
+        self._pokedexId = pokedexId
+    }
+}
