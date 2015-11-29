c7b95d9021f27381a21558950f8d6eda871f3fef
Gene McCullagh <gene@lightroomsecrets.com>
2015-11-29 07:26:26 -0600

    Added background image and custom Collection Cell class

---
+++ pokedex/PokeCell.swift
@@ -0,0 +1,23 @@
+//
+//  PokeCell.swift
+//  pokedex
+//
+//  Created by Gene McCullagh on 11/29/15.
+//  Copyright © 2015 Gene McCullagh. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    @IBOutlet weak var thumbImg: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    
+    var pokemon: Pokemon!
+    
+    func configureCell(pokemon: Pokemon) {
+        self.pokemon = pokemon
+        
+        nameLbl.text = self.pokemon.name.capitalizedString
+        thumbImg.image = UIImage(named: "\(self.pokemon.pokedexId)")
+    }
+}
